f2ae80547d3867e5eea8077d9b07f02765e5f123
dw-right <578486558@qq.com>
2018-01-15 20:35:21 +0800

    dsfdsd

---
+++ src/main/java/sendnote/App.java
@@ -0,0 +1,13 @@
+package sendnote;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
